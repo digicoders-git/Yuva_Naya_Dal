884f414f1e84f2a3d918c41b678c780d45ce0d7c
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-04-21 11:39:09 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
-import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70 } from '../utils/images';
+import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70, image71,image72, image73, image74  } from '../utils/images';
 import { HiOutlineShieldCheck, HiOutlineGlobe, HiOutlineChartBar, HiOutlineArrowRight } from 'react-icons/hi';
 import { FaBalanceScale, FaGraduationCap, FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi';
@@ -1348,6 +1348,46 @@ const Home = () => {
                             delay={0.1}
                             position="object-top object-contain"
                             onClick={() => setSelectedImage({ img: image67, name: "उत्तम यादव" })}
+                        />
+
+                        <LeaderCard 
+                            image={image71}
+                            name="दीपक कुमार"
+                            title="जिला उपाध्यक्ष"
+                            subtitle="बनारस, उत्तर प्रदेश"
+                            delay={0.1}
+                            position="object-top object-contain"
+                            onClick={() => setSelectedImage({ img: image71, name: "दीपक कुमार" })}
+                        />
+
+                         <LeaderCard 
+                            image={image72}
+                            name="मुकेश पाण्डेय"
+                            title="सादर विधानसभा अध्यक्ष"
+                            subtitle="फतेहपुर , उत्तर प्रदेश"
+                            delay={0.1}
+                            position="object-top object-contain"
+                            onClick={() => setSelectedImage({ img: image72, name: "मुकेश पाण्डेय" })}
+                        />
+
+                         <LeaderCard 
+                            image={image73}
+                            name="दिलीप कुमार पासी"
+                            title="विधानसभा अध्यक्ष , अयाह शाह"
+                            subtitle="फतेहपुर , उत्तर प्रदेश"
+                            delay={0.1}
+                            position="object-top object-contain"
+                            onClick={() => setSelectedImage({ img: image73, name: "दिलीप कुमार पासी" })}
+                        />
+
+                         <LeaderCard 
+                            image={image74}
+                            name="आर्यन यादव"
+                            title="जिला सचिव"
+                            subtitle="कानपुर देहात , उत्तर प्रदेश"
+                            delay={0.1}
+                            position="object-top object-contain"
+                            onClick={() => setSelectedImage({ img: image74, name: "आर्यन यादव" })}
                         />
 
 
