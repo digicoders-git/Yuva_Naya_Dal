d99e968202d23bd858d0d5449349a04aa6be9c92
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-05-25 13:06:15 +0530

    all done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
-import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70, image71,image72, image73, image74, image75, image76, image77, image78, image79, image80, image81, image82, image83, image84,image85 ,image86, image87, image88, image89, image90, image91, image92 , image93 , image94, image95, image96, image97, image98, image99} from '../utils/images';
+import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70, image71,image72, image73, image74, image75, image76, image77, image78, image79, image80, image81, image82, image83, image84,image85 ,image86, image87, image88, image89, image90, image91, image92 , image93 , image94, image95, image96, image97, image98, image99, image100} from '../utils/images';
 import { HiOutlineShieldCheck, HiOutlineGlobe, HiOutlineChartBar, HiOutlineArrowRight } from 'react-icons/hi';
 import { FaBalanceScale, FaGraduationCap, FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi';
@@ -866,15 +866,7 @@ const Home = () => {
                                     onClick={() => setSelectedImage({ img: image44, name: "राम सिंह यादव" })}
                                 />
 
-                                <LeaderCard
-                                    image={image99}
-                                    name="अभिषेक सिंह"
-                                    title="राष्ट्रीय उपाध्यक्ष"
-                                    subtitle="उत्तर प्रदेश"
-                                    delay={0.6}
-                                    position="object-top object-contain"
-                                    onClick={() => setSelectedImage({ img: image99, name: "अभिषेक सिंह" })}
-                                />
+                              
                             </div>
                         </motion.div>
 
@@ -1306,6 +1298,16 @@ const Home = () => {
                             onClick={() => setSelectedImage({ img: image95, name: "विजय कुमार साहू" })}
                         />
 
+                         <LeaderCard   
+                                    image={image100}
+                                    name="मयंक सिंह"
+                                    title="उपाध्यक्ष"
+                                    subtitle="उत्तर प्रदेश"
+                                    delay={0.3}
+                                    position="object-top object-contain"
+                                    onClick={() => setSelectedImage({ img: image100, name: "मयंक सिंह" })}
+                                />
+
                         <LeaderCard
                             image={image91}
                             name="संजय कुमार"
@@ -1338,12 +1340,27 @@ const Home = () => {
                         <LeaderCard
                             image={image97}
                             name="अमित कुमार जी"
-                            title="महासचिव"
+                            title="सचिव"
                             subtitle="उत्तर प्रदेश"
                             delay={0.3}
                             position="object-top object-contain"
                             onClick={() => setSelectedImage({ img: image97, name: "अमित कुमार जी" })}
                         />
+
+
+                         <LeaderCard   
+                                    image={image99}
+                                    name="अभिषेक सिंह"
+                                    title="महासचिव"
+                                    subtitle="उत्तर प्रदेश"
+                                    delay={0.3}
+                                    position="object-top object-contain"
+                                    onClick={() => setSelectedImage({ img: image99, name: "अभिषेक सिंह" })}
+                                />
+                         
+
+                         
+                         
                     </div>
                 </div>
             </section>
@@ -1714,7 +1731,7 @@ const Home = () => {
                             onClick={() => setSelectedImage({ img: image80, name: "मोहम्मद आदिल" })}
                         />
 
-                         
+                            
 
 
 
