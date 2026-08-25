314377c5af515a9e5e2e548b6234d783668b16c3
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-08-25 18:28:56 +0530

    feat: add image-118 and update gallery assets utility and Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
-import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70, image71,image72, image73, image74, image75, image76, image77, image78, image79, image80, image81, image82, image83, image84,image85 ,image86, image87, image88, image89, image90, image91, image92 , image93 , image94, image95, image96, image97, image98, image99, image100, image101, image103, image104, image105,image106, image107,image108, image109, image110, image111, image112, image113, image114, image115, image116, image117} from '../utils/images';
+import { logo, image36, image37, image38, image8 , image40 , image41 ,image42 , image43, image44, image45, image47, image48, image49, image50, image51, image52, image53, image46, image54,image55, image56, image57, image58, image59, image60, image61, image62, image63, image64, image65, image66, image67, image68, image69, image70, image71,image72, image73, image74, image75, image76, image77, image78, image79, image80, image81, image82, image83, image84,image85 ,image86, image87, image88, image89, image90, image91, image92 , image93 , image94, image95, image96, image97, image98, image99, image100, image101, image103, image104, image105,image106, image107,image108, image109, image110, image111, image112, image113, image114, image115, image116, image117, image118} from '../utils/images';
 import { HiOutlineShieldCheck, HiOutlineGlobe, HiOutlineChartBar, HiOutlineArrowRight } from 'react-icons/hi';
 import { FaBalanceScale, FaGraduationCap, FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi';
@@ -1092,6 +1092,25 @@ const Home = () => {
                                     delay={0.6}
                                     position="object-top object-contain"
                                     onClick={() => setSelectedImage({ img: image110, name: "श्री मनोज जी," })}
+                                />
+
+
+                                 {/* Connector */}
+                                <div className="flex flex-col items-center my-1">
+                                    <div className="w-0.5 h-8 bg-gradient-to-b from-navy-flag to-saffron opacity-40"/>
+                                    <div className="w-3 h-3 rounded-full bg-navy-flag/60"/>
+                                </div>
+
+                                <LeaderCard
+                                    image={image118}
+                                    name="राज नारायण
+,"
+                                    title="फ़तेहपुर,सदस्य राष्ट्रीय कार्यकारिणी"
+                                    subtitle="उत्तर प्रदेश
+"
+                                    delay={0.6}
+                                    position="object-top object-contain"
+                                    onClick={() => setSelectedImage({ img: image118, name: "राज नारायण जी," })}
                                 />
                             
                         </motion.div>
